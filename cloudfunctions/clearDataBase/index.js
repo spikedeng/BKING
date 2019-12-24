f24e54fc38ef5206f2139f9ea23732c8ebe7ae39
Spike Deng <spikedeng@icloud.com>
2019-12-25 00:52:42 +0800

    增加触发发布时的通知

--- cloudfunctions/clearDataBase/index.js
+++ cloudfunctions/clearDataBase/index.js
@@ -8,7 +8,7 @@ exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
   const _ = cloud.database().command
   await cloud.database().collection('messages').where({OPENID: _.exists(true)}).remove()
-  await cloud.database().collection('refines').where({OPENID: _.exists(true)}).remove()
+  // await cloud.database().collection('refines').where({OPENID: _.exists(true)}).remove()
   return {
     event,
     openid: wxContext.OPENID,
--- cloudfunctions/lightDigest/index.js
+++ cloudfunctions/lightDigest/index.js
@@ -15,15 +15,30 @@ exports.main = async(event, context) => {
   const digestData = await digest.get()
   const {
     published,
-    lights
+    lights,
+    OPENID
   } = digestData.data
   const needPublish = published === false && lights === 3 && operation === 'on'
+  if (needPublish) {
+    const digestIdTxt = digestId.slice(digestId.length - 6, digestId.length - 1)
+    console.log('param', OPENID, digestId)
+    cloud.callFunction({
+      name: 'addMessage',
+      data: {
+        OPENID,
+        content: `恭喜ID为${digestIdTxt}的投稿被选为精选！`,
+        digestId
+      }
+    })
+  }
   const lightresp = await digest.update({
     data: {
       lights: _.inc(operation === 'off' ? -1 : 1),
-      published: needPublish? true: published
+      published: needPublish ? true : published
     }
   })
+
+
   await cloud.callFunction({
     name: 'addLight',
     data: {
